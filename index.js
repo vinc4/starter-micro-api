11cd5a5340f9e262d7d8df7133c100250ee6fdaa
vincent map <vincentmap@gmail.com>
2023-12-27 14:09:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // MongoDB connection URI
-const mongoURI = "mongodb+srv://munashemap95:2NQrwHdnBmSx8OdE@cluster0.d494s66.mongodb.net/?retryWrites=true&w=majority11";
+const mongoURI = "mongodb+srv://munashemap95:2NQrwHdnBmSx8OdE@cluster0.d494s66.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(mongoURI);
+
+// Test API endpoint to check MongoDB connection status
+app.get("/test-connection", async (req, res) => {
+    try {
+        await client.connect();
+        res.json({ status: 'MongoDB connection is successful.' });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        res.status(500).json({ error: 'MongoDB connection failed.' });
+    }
+});
 
 app.get("/items/:my_item", async (req, res) => {
     let my_item = req.params.my_item;
-    let item = await client.db("test")
-                .collection("searchResults")
-                .findOne({my_item: my_item})
+    let item = await client.db("my_db")
+                .collection("my_collection")
+                .findOne({ my_item: my_item });
 
     return res.json(item);
 });
