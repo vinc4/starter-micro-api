f0cd273d8f74755f9ab85acfa9c9fa73b5f9f127
vincent map <vincentmap@gmail.com>
2023-12-27 13:58:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const { MongoClient } = require('mongodb');
-
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
-
 // MongoDB connection URI
 const mongoURI = "mongodb+srv://munashemap95:2NQrwHdnBmSx8OdE@cluster0.d494s66.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(mongoURI);
 
-let client;
-let db;
+app.get("/items/:my_item", async (req, res) => {
+    let my_item = req.params.my_item;
+    let item = await client.db("test")
+                .collection("searchResults")
+                .findOne({my_item: my_item})
 
-async function connectToDatabase() {
-  try {
-    client = await MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-    db = client.db('test');
-    console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1);
-  }
-}
-
-connectToDatabase();
-
-// Test database connection endpoint
-app.get('/test-connection', (req, res) => {
-  if (db) {
-    res.json({ message: 'MongoDB connection is successful.' });
-  } else {
-    res.status(500).json({ error: 'MongoDB connection failed.' });
-  }
+    return res.json(item);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+client.connect(err => {
+    if (err) {
+        console.error('Error connecting to MongoDB:', err);
+        process.exit(1);
+    }
+    // Connection to MongoDB is successful, listen for requests
+    app.listen(PORT, () => {
+        console.log("Server is running on http://localhost:" + PORT);
+    });
 });
